2953767fe1c823190ab2e36560f493a18f9edf68
NabillaGonibala <saputrafarel2003@gmail.com>
2024-12-06 01:22:06 +0800

    Add files via upload

---
+++ Build/home.php
@@ -0,0 +1,12 @@
+ <div class="col-lg-10 mt-2">
+                <div class="card">
+                    <div class="card-header">
+                        Home
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Bagian Home</h5>
+                        <p class="card-text">“Hidup dapat diibaratkan seperti minum kopi suguhan. Terkadang, kita akan mendapatkan rasa pahit, terkadang kita juga mendapatkan rasa manis. Namun, hal itu akan membuat mata kita menjadi terbuka.”</p>
+                        <a href="#" class="btn btn-primary">Go somewhere </a>
+                    </div>
+                </div>
+            </div>
